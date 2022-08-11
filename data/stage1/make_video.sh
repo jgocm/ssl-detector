ad2690ccb946a40c0be07305637951fd62790155
joao guilherme melo <jgocm@cin.ufpe.br>
2022-08-11 15:58:39 -0300

    upload videos and gifs

---
+++ data/stage1/make_video.sh
@@ -0,0 +1,1 @@
+ffmpeg -f image2 -i frame%d.jpg stage1.mp4
--- data/stage2/make_video.sh
+++ data/stage2/make_video.sh
@@ -1,1 +1,1 @@
-ffmpeg -f image2 -i /home/vision-blackout/ssl-detector/data/stage2/frame%d.jpg stage2.mp4
+ffmpeg -f image2 -i frame%d.jpg stage2.mp4
